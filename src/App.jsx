1628142a6f047fd343db0abc5789984f4492fffc
SaqibSattar <saqibkhanz1234@gmail.com>
2024-02-08 11:41:04 +0400

    Global Filtering

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import { BasicTable } from './components/BasicTable'
 import { SortingTable } from './components/SortingTable'
+import { FilteringTable } from './components/FilteringTable'
 
 function App() {
 
   return (
     <>
-      {/* <BasicTable /> */}
-      <SortingTable />
+      {/* <BasicTable />
+      <SortingTable /> */}
+      <FilteringTable />
     </>
   )
 }
---
+++ src/components/FilteringTable.jsx
@@ -0,0 +1,74 @@
+import React, { useMemo } from 'react';
+import { useTable, useGlobalFilter } from 'react-table';
+import MOCK_DATA from './MOCK_DATA.json';
+import { COLUMNS } from './columns';
+import './table.css';
+import { GlobalFilter } from './GlobalFilter';
+
+export const FilteringTable = () => {
+  const columns = useMemo(() => COLUMNS, []);
+  const data = useMemo(() => MOCK_DATA, []);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    footerGroups,
+    rows,
+    prepareRow,
+    state, // Ensure you are using the state object
+    setGlobalFilter, // Ensure you are using the setGlobalFilter function
+  } = useTable(
+    {
+      columns,
+      data,
+    },
+    useGlobalFilter
+  );
+
+  const { globalFilter } = state;
+
+  return (
+    <>
+      <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr key={headerGroup.id} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th key={column.id} {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr key={row.id} {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td key={cell.column.id} {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {footerGroups.map((footerGroup) => (
+            <tr key={footerGroup.id} {...footerGroup.getFooterGroupProps()}>
+              {footerGroup.headers.map((column) => (
+                <td key={column.id} {...column.getFooterProps()}>
+                  {column.render('Footer')}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
+      </table>
+    </>
+  );
+};
